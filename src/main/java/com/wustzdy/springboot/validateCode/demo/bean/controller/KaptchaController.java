48627906b86384b4ba03dcc8b8c9807819a22aa1
zhudayang <zhudayang@sensetime.com>
2021-07-07 11:13:13 +0800

    图形验证码

---
+++ src/main/java/com/wustzdy/springboot/validateCode/demo/bean/controller/KaptchaController.java
@@ -0,0 +1,116 @@
+package com.wustzdy.springboot.validateCode.demo.bean.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class KaptchaController {
+    private HttpServletRequest request;
+    private HttpServletResponse response;
+
+    // 生成验证码图片
+    @RequestMapping("/getCaptchaImage")
+    @ResponseBody
+    public void getCaptcha(HttpServletRequest request, HttpServletResponse response) {
+        this.request = request;
+        this.response = response;
+
+        try {
+
+            response.setContentType("image/png");
+            response.setHeader("Cache-Control", "no-cache");
+            response.setHeader("Expire", "0");
+            response.setHeader("Pragma", "no-cache");
+
+            ValidateCode validateCode = new ValidateCode();
+
+            // 直接返回图片
+            validateCode.getRandomCodeImage(request, response);
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    // 生成验证码,返回的是 base64
+    @RequestMapping("/getCaptchaBase64")
+    @ResponseBody
+    public String getCaptchaBase64(HttpServletRequest request, HttpServletResponse response) {
+
+        Map result = new HashMap();
+        try {
+
+            response.setContentType("image/png");
+            response.setHeader("Cache-Control", "no-cache");
+            response.setHeader("Expire", "0");
+            response.setHeader("Pragma", "no-cache");
+
+            ValidateCode validateCode = new ValidateCode();
+
+            // 直接返回图片
+            // validateCode.getRandomCode(request, response);
+
+            // 返回base64
+            String base64String = validateCode.getRandomCodeBase64(request, response);
+            result.put("url", "data:image/png;base64," + base64String);
+            result.put("message", "created successfull");
+            System.out.println("test=" + result.get("url"));
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        return (String) result.get("url");
+    }
+
+    /**
+     * 校对验证码
+     */
+    @RequestMapping("/Validate")
+    public ModelAndView imgvrifyControllerDefaultKaptcha(HttpServletRequest request,String tryCode) {
+        ModelAndView model = new ModelAndView();
+        String rightCode = (String) request.getSession().getAttribute("rightCode");
+        System.out.println("rightCode:" + rightCode + " ———— tryCode:" + tryCode);
+        if (!rightCode.equals(tryCode)) {
+            model.addObject("info", "验证码错误,请再输一次!");
+            model.setViewName("login");
+        } else {
+            model.addObject("info", "登陆成功");
+            model.setViewName("index");
+        }
+        return model;
+    }
+
+    /**
+     * 返回首页
+     */
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView index() {
+        return new ModelAndView("login");
+    }
+
+    @RequestMapping("/ValidateCode")
+    public Boolean ValidateCode(HttpServletRequest request, String code) {
+        String validateCode = request.getSession().getAttribute("rightCode").toString();
+        if (validateCode.equals(code)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
